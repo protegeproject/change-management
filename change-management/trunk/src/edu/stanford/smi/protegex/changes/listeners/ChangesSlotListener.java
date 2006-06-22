7575cc7ed42335c14d5b4d0176279148cd0cb788
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-06-22 20:53:59 +0000

    removed license text from top of source files.  we have a script that prepends this text to each file during the build process.  nothing should go into subversion with license text already in it.

--- change-management/trunk/src/edu/stanford/smi/protegex/changes/listeners/ChangesSlotListener.java
+++ change-management/trunk/src/edu/stanford/smi/protegex/changes/listeners/ChangesSlotListener.java
@@ -1,26 +1,3 @@
-/*
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License");  you may not use this file except in 
- * compliance with the License.  You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is Protege-2000.
- *
- * The Initial Developer of the Original Code is Stanford University. Portions
- * created by Stanford University are Copyright (C) 2005.  All Rights Reserved.
- *
- * Protege was developed by Stanford Medical Informatics
- * (http://www.smi.stanford.edu) at the Stanford University School of Medicine
- * with support from the National Library of Medicine, the National Science
- * Foundation, and the Defense Advanced Research Projects Agency.  Current
- * information about Protege can be obtained at http://protege.stanford.edu.
- *
- */
-
 package edu.stanford.smi.protegex.changes.listeners;
 
 import edu.stanford.smi.protege.event.SlotEvent;
