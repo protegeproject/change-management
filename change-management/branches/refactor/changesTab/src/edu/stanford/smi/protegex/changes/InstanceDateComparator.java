a5771d3db4e0eecc8c1151a68e14da31d03625be
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-17 02:24:47 +0000

    added check for null time value of one object

--- change-management/branches/refactor/changesTab/src/edu/stanford/smi/protegex/changes/InstanceDateComparator.java
+++ change-management/branches/refactor/changesTab/src/edu/stanford/smi/protegex/changes/InstanceDateComparator.java
@@ -16,16 +16,18 @@ public class InstanceDateComparator implements Comparator{
 	public int compare(Object obj1, Object obj2) {
 		
 		int value = 0;
-		
+				
 		if (obj1 instanceof Instance && obj2 instanceof Instance) {
 			Instance inst1 = (Instance) obj1;
 			Instance inst2 = (Instance) obj2;
 			
 			String time1 = ChangeCreateUtil.getCreated(changeKb, inst1);
 			String time2 = ChangeCreateUtil.getCreated(changeKb, inst2);
-			
+					
 			if (time2 == null) {
 				value = 0;
+			} else if (time1 == null) {
+				value = 1;
 			} else {
 				value = time1.compareTo(time2);
 			}
