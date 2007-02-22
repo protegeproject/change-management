b7e886466eb5a10e9545e37705608cd7cc0c404d
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-22 20:56:41 +0000

    de-staticized

--- change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesClassListener.java
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesClassListener.java
@@ -1,14 +1,26 @@
 package edu.stanford.smi.protegex.server_changes.listeners.owl;
 
+import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.Instance;
-import edu.stanford.smi.protegex.server_changes.*;
-import edu.stanford.smi.protegex.server_changes.util.Util;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.model.RDFSClass;
 import edu.stanford.smi.protegex.owl.model.event.ClassAdapter;
+import edu.stanford.smi.protegex.server_changes.ChangesDb;
+import edu.stanford.smi.protegex.server_changes.ChangesProject;
+import edu.stanford.smi.protegex.server_changes.ServerChangesUtil;
+import edu.stanford.smi.protegex.server_changes.util.Util;
  
 public class OwlChangesClassListener extends ClassAdapter{
+    private OWLModel om;
+    private KnowledgeBase changesKb;
+    
+    public OwlChangesClassListener(OWLModel om) {
+        this.om = om;
+        changesKb = ChangesProject.getChangesKB(om);
+    }
 
 	public void instanceAdded(RDFSClass arg0, RDFResource arg1) {
 		String instName = arg1.getBrowserText();
@@ -20,22 +32,19 @@ public class OwlChangesClassListener extends ClassAdapter{
 		context.append(")");
 		
 		// Update frames map
-		String frameId = arg0.getFrameID().toString();
-		if (!Util.frameExists(frameId)) {
-			Util.updateMap(frameId, instName);	
-		}
-		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_INSTANCE_ADDED,
 												instName, 
 												context.toString(), 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	public void instanceRemoved(RDFSClass arg0, RDFResource arg1) {
-		String instName = Util.getName(arg1.getFrameID().toString());
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
+		String instName = changesDb.getName(arg1);
 		StringBuffer context = new StringBuffer();
 		context.append("Instance Removed: ");
 		context.append(instName);
@@ -43,13 +52,13 @@ public class OwlChangesClassListener extends ClassAdapter{
 		context.append(arg0.getBrowserText());
 		context.append(")");
 		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_INSTANCE_REMOVED,
 												instName, 
 												context.toString(), 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	public void addedToUnionDomainOf(RDFSClass arg0, RDFProperty arg1) {
@@ -68,22 +77,20 @@ public class OwlChangesClassListener extends ClassAdapter{
 		context.append(")");
 		
 		// Update frames map
-		String frameId = arg1.getFrameID().toString();
-		if (!Util.frameExists(frameId)) {
-			Util.updateMap(frameId, clsName);	
-		}
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
 		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_SUBCLASS_ADDED,
 												clsName, 
 												context.toString(), 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	public void subclassRemoved(RDFSClass arg0, RDFSClass arg1) {
-		String clsName = Util.getName(arg1.getFrameID().toString());
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
+		String clsName = changesDb.getName(arg1);
 		StringBuffer context = new StringBuffer();
 		context.append("Subclass Removed: ");
 		context.append(clsName);
@@ -91,13 +98,13 @@ public class OwlChangesClassListener extends ClassAdapter{
 		context.append(arg0.getBrowserText());
 		context.append(")");
 		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_SUBCLASS_REMOVED,
 												clsName, 
 												context.toString(), 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	public void superclassAdded(RDFSClass arg0, RDFSClass arg1) {
@@ -110,24 +117,22 @@ public class OwlChangesClassListener extends ClassAdapter{
 		context.append(")");
 		
 		// Update frames map
-		String frameId = arg1.getFrameID().toString();
-		if (!Util.frameExists(frameId)) {
-			Util.updateMap(frameId, clsName);	
-		}
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
 		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_SUPERCLASS_ADDED,
 												clsName, 
 												context.toString(),
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	public void superclassRemoved(RDFSClass arg0, RDFSClass arg1) {
 		String a = arg1.getName();
 		String b = arg1.getBrowserText();
-		String clsName = Util.getName(arg1.getFrameID().toString());
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
+		String clsName = changesDb.getName(arg1);
 		StringBuffer context = new StringBuffer();
 		context.append("Superclass Removed: ");
 		context.append(clsName);
@@ -135,12 +140,12 @@ public class OwlChangesClassListener extends ClassAdapter{
 		context.append(arg0.getBrowserText());
 		context.append(")");
 		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_SUPERCLASS_REMOVED,
 												clsName, 
 												context.toString(), 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 }
--- change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesModelListener.java
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesModelListener.java
@@ -1,32 +1,41 @@
 package edu.stanford.smi.protegex.server_changes.listeners.owl;
 
-import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.Instance;
-import edu.stanford.smi.protegex.server_changes.*;
-import edu.stanford.smi.protegex.server_changes.util.Util;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.model.RDFSClass;
 import edu.stanford.smi.protegex.owl.model.event.ModelAdapter;
+import edu.stanford.smi.protegex.server_changes.ChangesDb;
+import edu.stanford.smi.protegex.server_changes.ChangesProject;
+import edu.stanford.smi.protegex.server_changes.ServerChangesUtil;
+import edu.stanford.smi.protegex.server_changes.util.Util;
 
 public class OwlChangesModelListener extends ModelAdapter{
+    private OWLModel om;
+    private KnowledgeBase changesKb;
+    
+    
+    public OwlChangesModelListener(OWLModel om) {
+        this.om = om;
+        changesKb = ChangesProject.getChangesKB(om);
+    }
 
 	public void classCreated(RDFSClass arg0) {
 		String clsName = arg0.getBrowserText();
 		String context = "Created Class: " + clsName;
         
-		String frameId = arg0.getFrameID().toString();
-		if (!Util.frameExists(frameId)) {
-			Util.updateMap(frameId, clsName);	
-		}
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
 				
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_CLASS_CREATED,
 												clsName, 
 												context, 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	
@@ -60,38 +69,35 @@ public class OwlChangesModelListener extends ModelAdapter{
 		String propName = arg0.getBrowserText();
 		String context = "Property Created: " + propName;
 
-		String frameId = arg0.getFrameID().toString();
-		if (!Util.frameExists(frameId)) {
-			Util.updateMap(frameId, propName);
-		}
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
 
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_PROPERTY_CREATED,
 												propName, 
 												context, 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	}
 
 	public void propertyDeleted(RDFProperty arg0) {
-		String propName = Util.getName(arg0.getFrameID().toString());
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
+		String propName = changesDb.getName(arg0);
 		String context = "Property Deleted: " + propName;
 		
-		Instance changeInst = ServerChangesUtil.createChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_PROPERTY_DELETED,
 												propName, 
 												context, 
 												ServerChangesUtil.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om,changesKb, changeInst);
 	
 	}
 
 	public void resourceNameChanged(RDFResource arg0, String arg1) {
 		String oldName = arg1;
 		String newName = arg0.getBrowserText();
-		String frameId = arg0.getFrameID().toString();
 
 		StringBuffer context = new StringBuffer();
 		context.append("Name change from '");
@@ -100,17 +106,17 @@ public class OwlChangesModelListener extends ModelAdapter{
 		context.append(newName);
 		context.append("'");
 		
-		Util.updateMap(frameId, newName);
+        ChangesDb changesDb = ChangesProject.getChangesDb(om);
 		
-		Instance changeInst = ServerChangesUtil.createNameChange(
-												ChangesProject.getChangesKB(),
+		Instance changeInst = ServerChangesUtil.createNameChange(om,
+												changesKb,
 												ServerChangesUtil.CHANGETYPE_NAME_CHANGED,
 												newName, 
 												context.toString(), 
 												ServerChangesUtil.CHANGE_LEVEL_INFO, 
 												oldName, 
 												newName);
-		ChangesProject.createChange(changeInst);
+		ChangesProject.createChange(om, changesKb, changeInst);
 		
 	}
 }
