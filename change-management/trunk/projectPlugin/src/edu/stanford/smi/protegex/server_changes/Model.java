aac84cd98cd8657b3079508100db24c323338322
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-24 01:52:39 +0000

    coming soon.

---
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/Model.java
@@ -0,0 +1,13 @@
+package edu.stanford.smi.protegex.server_changes;
+
+import edu.stanford.smi.protege.model.KnowledgeBase;
+
+public class Model {
+    private KnowledgeBase changesKB;
+    
+    
+    public Model(KnowledgeBase changesKB) {
+        this.changesKB = changesKB;
+    }
+
+}
