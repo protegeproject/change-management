c16bfe6f0526cbd140fb67e2473d3ea625384cd4
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-26 05:33:56 +0000

    refactored code that should be the same in both the frames and owl world and removed unused code.

--- change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesModelListener.java
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesModelListener.java
@@ -17,82 +17,81 @@ import edu.stanford.smi.protegex.server_changes.util.Util;
 public class OwlChangesModelListener extends ModelAdapter{
     private OWLModel om;
     private KnowledgeBase changesKb;
-    
-    
+
+
     public OwlChangesModelListener(OWLModel om) {
         this.om = om;
         changesKb = ChangesProject.getChangesKB(om);
     }
 
-	public void classCreated(RDFSClass arg0) {
-		String clsName = arg0.getBrowserText();
-		String context = "Created Class: " + clsName;
-        
-        ChangesDb changesDb = ChangesProject.getChangesDb(om);
-				
-		Instance changeInst = ServerChangesUtil.createChange(om,
-												changesKb,
-												Model.CHANGETYPE_CLASS_CREATED,
-												clsName, 
-												context, 
-												Model.CHANGE_LEVEL_INFO);
-		ChangesProject.createChange(om,changesKb, changeInst);
-	}
+    public void classCreated(RDFSClass arg0) {
+        String clsName = arg0.getBrowserText();
+        String context = "Created Class: " + clsName;
+        Instance changeInst = ServerChangesUtil.createChange(om,
+                                                             changesKb,
+                                                             Model.CHANGETYPE_CLASS_CREATED,
+                                                             clsName, 
+                                                             context, 
+                                                             Model.CHANGE_LEVEL_INFO);
+        ChangesProject.createChange(om,changesKb, changeInst);
+    }
 
-	
-	
 
-	public void classDeleted(RDFSClass arg0) {
-	    // This can't be done here because of the missing name field.  Look in the kb listener.
-	}
 
-	public void individualCreated(RDFResource arg0) {
-	}
 
-	public void individualDeleted(RDFResource arg0) {
-	}
+    public void classDeleted(RDFSClass arg0) {
+        // This can't be done here because of the missing name field.  Look in the kb listener.
+    }
 
-	public void propertyCreated(RDFProperty arg0) {
+    public void individualCreated(RDFResource arg0) {
+    }
+
+    public void individualDeleted(RDFResource arg0) {
+    }
+
+    public void propertyCreated(RDFProperty arg0) {
         String propName = arg0.getBrowserText();
         String context = "Property Created: " + propName;
 
         ChangesDb changesDb = ChangesProject.getChangesDb(om);
 
         Instance changeInst = ServerChangesUtil.createChange(om,
-                                                changesKb,
-                                                Model.CHANGETYPE_PROPERTY_CREATED,
-                                                propName, 
-                                                context, 
-                                                Model.CHANGE_LEVEL_INFO);
+                                                             changesKb,
+                                                             Model.CHANGETYPE_PROPERTY_CREATED,
+                                                             propName, 
+                                                             context, 
+                                                             Model.CHANGE_LEVEL_INFO);
         ChangesProject.createChange(om,changesKb, changeInst);
-	}
+    }
 
-	public void propertyDeleted(RDFProperty arg0) {
+    public void propertyDeleted(RDFProperty arg0) {
         // This can't be done here because of the missing name field.  Look in the kb listener.
-	}
+    }
 
-	public void resourceNameChanged(RDFResource arg0, String arg1) {
-		String oldName = arg1;
-		String newName = arg0.getBrowserText();
+    public void resourceNameChanged(RDFResource arg0, String arg1) {
+        String oldName = arg1;
+        String newName = arg0.getName();
+        
+        ServerChangesUtil.updateChangeDbAfterNameChange(changesKb, oldName, newName);
 
-		StringBuffer context = new StringBuffer();
-		context.append("Name change from '");
-		context.append(oldName);
-		context.append("' to '");
-		context.append(newName);
-		context.append("'");
-		
+        StringBuffer context = new StringBuffer();
+        context.append("Name change from '");
+        context.append(oldName);
+        context.append("' to '");
+        context.append(newName);
+        context.append("'");
+
         ChangesDb changesDb = ChangesProject.getChangesDb(om);
-		
-		Instance changeInst = ServerChangesUtil.createNameChange(om,
-												changesKb,
-												Model.CHANGETYPE_NAME_CHANGED,
-												newName, 
-												context.toString(), 
-												Model.CHANGE_LEVEL_INFO, 
-												oldName, 
-												newName);
-		ChangesProject.createChange(om, changesKb, changeInst);
-		
-	}
+
+        Instance changeInst = ServerChangesUtil.createNameChange(om,
+                                                                 changesKb,
+                                                                 Model.CHANGETYPE_NAME_CHANGED,
+                                                                 newName, 
+                                                                 context.toString(), 
+                                                                 Model.CHANGE_LEVEL_INFO, 
+                                                                 oldName, 
+                                                                 newName);
+        ChangesProject.createChange(om, changesKb, changeInst);
+
+    }
 }
