3063e572d00bc9c6361a5b38b26905cb63729116
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-03-03 17:06:30 +0000

    a version of the diff user tab on this side.

---
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/prompt/DiffUserTab.java
@@ -0,0 +1,13 @@
+package edu.stanford.smi.protegex.server_changes.prompt;
+
+import edu.stanford.smi.protege.widget.AbstractTabWidget;
+
+public class DiffUserTab extends AbstractTabWidget {
+
+    public void initialize() {
+        // Set the title for the tab widget.
+        setLabel("Diff User Tab");
+        
+    }
+
+}
