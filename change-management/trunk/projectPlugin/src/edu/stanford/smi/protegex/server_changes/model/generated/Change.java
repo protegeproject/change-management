94e96b9bb50039eeacf59a09a7f3bb2bcb9e2bd9
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-03-05 04:20:28 +0000

    removed the type slot from Change

--- change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/generated/Change.java
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/generated/Change.java
@@ -1,19 +1,23 @@
 
-// Created on Sat Mar 03 09:41:37 PST 2007
+// Created on Sun Mar 04 20:08:38 PST 2007
 // "Copyright Stanford University 2006"
 
 package edu.stanford.smi.protegex.server_changes.model.generated;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import edu.stanford.smi.protege.model.DefaultSimpleInstance;
 import edu.stanford.smi.protege.model.FrameID;
 import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.ModelUtilities;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.server_changes.model.ChangeDateComparator;
 import edu.stanford.smi.protegex.server_changes.model.ChangeModel;
 import edu.stanford.smi.protegex.server_changes.model.ChangeModel.ChangeCls;
 
@@ -66,12 +70,6 @@ public abstract class Change extends AnnotatableThing {
 		ModelUtilities.setOwnSlotValue(this, "timestamp", timestamp);	}
 	public Instance getTimestamp() {
 		return ((Instance) ModelUtilities.getOwnSlotValue(this, "timestamp"));
-	}
-
-	public void setType(String type) {
-		ModelUtilities.setOwnSlotValue(this, "type", type);	}
-	public String getType() {
-		return ((String) ModelUtilities.getOwnSlotValue(this, "type"));
 	}
 // __Code above is automatically generated. Do not change
 }
