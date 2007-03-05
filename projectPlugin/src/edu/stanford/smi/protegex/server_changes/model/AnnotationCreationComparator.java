f4a49fe855be2bea4d1c57192e9b31e9c3a5125e
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-03-05 07:01:01 +0000

    changed the name of Timestamp.compareTo to avoid an understandable bug with over-riding DefaultFrame.compareTo.

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/AnnotationCreationComparator.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/AnnotationCreationComparator.java
@@ -11,7 +11,7 @@ public class AnnotationCreationComparator implements Comparator<Instance> {
     public int compare(Instance arg0, Instance arg1) {
         Annotation a0 = (Annotation) arg0;
         Annotation a1 = (Annotation) arg1;
-        return ((Timestamp) a0.getCreated()).compareTo((Timestamp) a1.getCreated());
+        return ((Timestamp) a0.getCreated()).compareTimestamp((Timestamp) a1.getCreated());
     }
 
 }
