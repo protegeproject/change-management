343c1e020c250554fa62fb9b9712fef1e2351828
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-03-07 23:57:54 +0000

    Minor changes to annotation properties and to make instances get the right applyTo.

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesFrameListener.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesFrameListener.java
@@ -13,11 +13,13 @@ import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.util.CollectionUtilities;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.impl.DefaultOWLDatatypeProperty;
 import edu.stanford.smi.protegex.server_changes.ChangesDb;
 import edu.stanford.smi.protegex.server_changes.ChangesProject;
 import edu.stanford.smi.protegex.server_changes.model.ChangeModel;
 import edu.stanford.smi.protegex.server_changes.model.ChangeModel.ChangeCls;
+import edu.stanford.smi.protegex.server_changes.model.generated.Annotation_Change;
 import edu.stanford.smi.protegex.server_changes.model.generated.Change;
 import edu.stanford.smi.protegex.server_changes.model.generated.Ontology_Component;
 
@@ -71,130 +73,140 @@ public class OwlChangesFrameListener implements FrameListener {
     public void ownSlotValueChanged(FrameEvent event) {
 
         Frame f = event.getFrame();
+        Slot slot = event.getSlot();
         if (f instanceof Slot) {
             // not handled yet...
         }
         else if (f instanceof Cls) {
 
             Cls c = (Cls)f;
-            Slot slot = event.getSlot();
-            String cText = c.getBrowserText();
-            String cName = c.getName();
-            Slot s = event.getSlot();
-            ArrayList oldSlotValues = (ArrayList) event.getOldValues();
-            String sName = s.getName();
-
-            StringBuffer context = new StringBuffer();
-
-            if(s instanceof DefaultOWLDatatypeProperty){
-                DefaultOWLDatatypeProperty sProp = (DefaultOWLDatatypeProperty)s;
-                boolean isAnnotation = sProp.isAnnotationProperty();
-
-                if(isAnnotation){
-                    Collection newSlotValues = c.getOwnSlotValues(slot);
-                    if ((newSlotValues == null && oldSlotValues  == null) || newSlotValues.equals(oldSlotValues)) {
-                        return;
-                    }
-                    if (newSlotValues == null || newSlotValues.isEmpty()){
-                        context.append("Annotation Removed: ");
-                        context.append(sName);
-                        context.append(" from class: ");
-                        context.append(cText);
-
-                        Ontology_Component applyTo = changes_db.getOntologyComponent(cName, true);
-
-                        Change change = changes_db.createChange(ChangeCls.Annotation_Removed);
-                        changes_db.finalizeChange(change, applyTo, context.toString());
-                    }//Annotation deleted
-                    else if (oldSlotValues == null || oldSlotValues.isEmpty()) {
-
-
-                        context.append("Annotation Added: ");
-                        context.append(sName);
-                        context.append(": ");
-
-                        context.append("'");
-                        context.append(CollectionUtilities.toString(newSlotValues));
-                        context.append("'");
-                        context.append(" to class: ");
-                        context.append(cName);
-
-                        Ontology_Component applyTo = changes_db.getOntologyComponent(cName, true);
-
-                        Change change = changes_db.createChange(ChangeCls.Annotation_Added);
-                        changes_db.finalizeChange(change, applyTo, context.toString());
-                    }
-                    else {
-                        context.append("Annotation Modified: ");
-                        context.append("annotation ");
-                        context.append(sName);
-                        context.append(" for class: ");
-                        context.append(cName);
-                        
-                        Ontology_Component applyTo = changes_db.getOntologyComponent(cName, true);
-                        
-                        Change change = changes_db.createChange(ChangeCls.Annotation_Modified);
-                        changes_db.finalizeChange(change, applyTo, context.toString());
-
-                    }
-                }// handles annotations
+ 
+            if (slot instanceof RDFProperty && ((RDFProperty) slot).isAnnotationProperty()) {
+                handleAnnotation(c, (RDFProperty) slot, event);
             }
-
-
-            if(sName.equals("owl:disjointWith")) { 
-                Collection newSlotValues = c.getOwnSlotValues(slot);
-                Collection deleted = new HashSet(oldSlotValues);
-                deleted.removeAll(newSlotValues);
-                Collection added = new HashSet(newSlotValues);
-                added.removeAll(oldSlotValues);
-                if (added.isEmpty() && deleted.isEmpty()) {
-                    return;
-                }
-                if (!added.isEmpty()) {
-                    context.append("Added disjoint class(es): ");
-                    context.append(CollectionUtilities.toString(added));
-                }
-                if (!deleted.isEmpty()) {
-                    context.append("removed disjoint class(es): ");
-                    context.append(CollectionUtilities.toString(deleted));
-                }
-                context.append(" to: ");
-                context.append(cName);
-                
-                Ontology_Component applyTo = changes_db.getOntologyComponent(cName, true);
-                
-                Change change = changes_db.createChange(ChangeCls.Disjoint_Class_Added);
-                changes_db.finalizeChange(change, applyTo, context.toString());
+            else if (slot.getName().equals("owl:disjointWith")) { 
+                handleOwlDisjoint(c, slot, event);
             } // Handles disjoints
 
         }
 
         else if (f instanceof Instance){
-            Instance i = (Instance)f;
-            String iName = i.getName();
-            String iText = i.getBrowserText();
-            Slot ownS = event.getSlot();
-            String ownSName = ownS.getName();
-            String newSlotValue = CollectionUtilities.toString(i.getOwnSlotValues(event.getSlot()));
-            ArrayList oldValue = (ArrayList)event.getArgument2();
-            String oldSlotValue = oldValue.toString();
-
-            StringBuffer context = new StringBuffer();
-            if(!ownSName.equals("rdf:type")) {
-                context.append("Slot: ");
-                context.append(ownSName);
-                context.append(" for Instance: ");
-                context.append(iText);
-                context.append(" set to: ");
-                context.append(newSlotValue);
-                
-                Ontology_Component applyTo = changes_db.getOntologyComponent(ownSName, true);
-                
-                Change change = changes_db.createChange(ChangeCls.Slot_Value);
-                changes_db.finalizeChange(change, applyTo, context.toString());
-            } 
+            if (!slot.getName().equals("rdf:type")) {
+                handleInstanceSlotValueChange((Instance) f, slot, event);
+            }
         }
 
+    }
+    
+
+    private void handleAnnotation(Cls c, RDFProperty prop, FrameEvent event) {
+        String cText = c.getBrowserText();
+        String cName = c.getName();
+        Slot s = event.getSlot();
+        ArrayList oldSlotValues = (ArrayList) event.getOldValues();
+        String sName = s.getName();
+
+        StringBuffer context = new StringBuffer();
+        Collection newSlotValues = c.getOwnSlotValues(prop);
+        if ((newSlotValues == null && oldSlotValues  == null) || newSlotValues.equals(oldSlotValues)) {
+            return;
+        }
+        
+        Ontology_Component applyTo = changes_db.getOntologyComponent(cName, true);
+        Ontology_Component ontologyAnnotation = changes_db.getOntologyComponent(sName, true);
+        
+        if (newSlotValues == null || newSlotValues.isEmpty()){
+            context.append("Annotation Removed: ");
+            context.append(sName);
+            context.append(" from class: ");
+            context.append(cText);
+
+            Annotation_Change change = (Annotation_Change) changes_db.createChange(ChangeCls.Annotation_Removed);
+            change.setOntologyAnnotation(ontologyAnnotation);
+            changes_db.finalizeChange(change, applyTo, context.toString());
+        }//Annotation deleted
+        else if (oldSlotValues == null || oldSlotValues.isEmpty()) {
+
+
+            context.append("Annotation Added: ");
+            context.append(sName);
+            context.append(": ");
+
+            context.append("'");
+            context.append(CollectionUtilities.toString(newSlotValues));
+            context.append("'");
+            context.append(" to class: ");
+            context.append(cName);
+
+            Annotation_Change change = (Annotation_Change) changes_db.createChange(ChangeCls.Annotation_Added);
+            change.setOntologyAnnotation(ontologyAnnotation);
+            changes_db.finalizeChange(change, applyTo, context.toString());
+        }
+        else {
+            context.append("Annotation Modified: ");
+            context.append("annotation ");
+            context.append(sName);
+            context.append(" for class: ");
+            context.append(cName);
+
+            Annotation_Change change = (Annotation_Change) changes_db.createChange(ChangeCls.Annotation_Modified);
+            change.setOntologyAnnotation(ontologyAnnotation);
+            changes_db.finalizeChange(change, applyTo, context.toString());
+        }
+    }
+    
+    private void handleOwlDisjoint(Cls c, Slot slot, FrameEvent event) {
+        String cText = c.getBrowserText();
+        String cName = c.getName();
+        Slot s = event.getSlot();
+        ArrayList oldSlotValues = (ArrayList) event.getOldValues();
+        String sName = s.getName();
+        StringBuffer context = new StringBuffer();
+        
+        Collection newSlotValues = c.getOwnSlotValues(slot);
+        Collection deleted = new HashSet(oldSlotValues);
+        deleted.removeAll(newSlotValues);
+        Collection added = new HashSet(newSlotValues);
+        added.removeAll(oldSlotValues);
+        if (added.isEmpty() && deleted.isEmpty()) {
+            return;
+        }
+        if (!added.isEmpty()) {
+            context.append("Added disjoint class(es): ");
+            context.append(CollectionUtilities.toString(added));
+        }
+        if (!deleted.isEmpty()) {
+            context.append("removed disjoint class(es): ");
+            context.append(CollectionUtilities.toString(deleted));
+        }
+        context.append(" to: ");
+        context.append(cName);
+        
+        Ontology_Component applyTo = changes_db.getOntologyComponent(cName, true);
+        
+        Change change = changes_db.createChange(ChangeCls.Disjoint_Class_Added);
+        changes_db.finalizeChange(change, applyTo, context.toString());
+    }
+    
+    private void handleInstanceSlotValueChange(Instance i, Slot ownS, FrameEvent event) {
+        String iName = i.getName();
+        String iText = i.getBrowserText();
+        String ownSName = ownS.getName();
+        String newSlotValue = CollectionUtilities.toString(i.getOwnSlotValues(event.getSlot()));
+        ArrayList oldValue = (ArrayList)event.getArgument2();
+
+        StringBuffer context = new StringBuffer();
+        context.append("Slot: ");
+        context.append(ownSName);
+        context.append(" for Instance: ");
+        context.append(iText);
+        context.append(" set to: ");
+        context.append(newSlotValue);
+
+        Ontology_Component applyTo = changes_db.getOntologyComponent(iName, true);
+
+        Change change = changes_db.createChange(ChangeCls.Slot_Value);
+        changes_db.finalizeChange(change, applyTo, context.toString());
     }
 
 
