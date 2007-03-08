5df4ba822535b4081c6c3fb116f63cb1259d94bd
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-03-08 00:40:44 +0000

    added annotation change

---
+++ model/src/edu/stanford/smi/protegex/server_changes/model/generated/Annotation_Change.java
@@ -0,0 +1,44 @@
+
+// Created on Wed Mar 07 13:03:39 PST 2007
+// "Copyright Stanford University 2006"
+
+package edu.stanford.smi.protegex.server_changes.model.generated;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import edu.stanford.smi.protege.model.DefaultSimpleInstance;
+import edu.stanford.smi.protege.model.FrameID;
+import edu.stanford.smi.protege.model.Instance;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.ModelUtilities;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.server_changes.model.ChangeDateComparator;
+import edu.stanford.smi.protegex.server_changes.model.ChangeModel;
+import edu.stanford.smi.protegex.server_changes.model.ChangeModel.ChangeCls;
+
+
+/** 
+ */
+public class Annotation_Change extends Class_Change {
+
+	public Annotation_Change() {
+	}
+
+
+	public Annotation_Change(KnowledgeBase kb, FrameID id ) {
+		super(kb, id);
+	}
+
+
+	public void setOntologyAnnotation(Instance ontologyAnnotation) {
+		ModelUtilities.setOwnSlotValue(this, "ontologyAnnotation", ontologyAnnotation);	}
+	public Instance getOntologyAnnotation() {
+		return ((Instance) ModelUtilities.getOwnSlotValue(this, "ontologyAnnotation"));
+	}
+// __Code above is automatically generated. Do not change
+}
