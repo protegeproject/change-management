4b51a9e390f63f47e359c5bb9fbdb07262866599
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-03-13 20:35:10 +0000

    added the modified change capability to the changes listener.

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/AbstractChangeListener.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/AbstractChangeListener.java
@@ -11,17 +11,25 @@ import edu.stanford.smi.protegex.server_changes.model.generated.Change;
 public abstract class AbstractChangeListener extends FrameAdapter {
     private Slot applyToSlot;
     private Slot annotatesSlot;
+    private Slot partOfCompositeChangeSlot;
     
     public AbstractChangeListener(ChangeModel model) {
         applyToSlot = model.getSlot(ChangeSlot.applyTo);
         annotatesSlot = model.getSlot(ChangeSlot.annotates);
+        partOfCompositeChangeSlot = model.getSlot(ChangeSlot.partOfCompositeChange);
     }
     
     public void ownSlotValueChanged(FrameEvent event) {
         Frame frame = event.getFrame();
         Slot slot = event.getSlot();
-        if (frame instanceof Change && slot.equals(applyToSlot)) {
-            addChange((Change)  frame);
+        if (frame instanceof Change) {
+            Change change = (Change) frame;
+            if (slot.equals(applyToSlot)) {
+                addChange(change);
+            }
+            else if (change.getApplyTo() != null  && !slot.equals(partOfCompositeChangeSlot)) {
+                modifyChange(change);
+            }
         }
         else if (frame instanceof Annotation && slot.equals(annotatesSlot)) {
             addAnnotation((Annotation) frame);
@@ -29,6 +37,8 @@ public abstract class AbstractChangeListener extends FrameAdapter {
     }
     
     public abstract void addChange(Change change);
+    
+    public abstract void modifyChange(Change change);
     
     public abstract void addAnnotation(Annotation annotation);
 
