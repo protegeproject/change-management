623cd47f5cfac1faf4cf6f08f1e52c74aa8be14d
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-03-17 00:39:17 +0000

    the modify change method now has slot and old value information

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/AbstractChangeListener.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/model/AbstractChangeListener.java
@@ -1,5 +1,7 @@
 package edu.stanford.smi.protegex.server_changes.model;
 
+import java.util.List;
+
 import edu.stanford.smi.protege.event.FrameAdapter;
 import edu.stanford.smi.protege.event.FrameEvent;
 import edu.stanford.smi.protege.model.Frame;
@@ -11,12 +13,10 @@ import edu.stanford.smi.protegex.server_changes.model.generated.Change;
 public abstract class AbstractChangeListener extends FrameAdapter {
     private Slot applyToSlot;
     private Slot annotatesSlot;
-    private Slot partOfCompositeChangeSlot;
     
     public AbstractChangeListener(ChangeModel model) {
         applyToSlot = model.getSlot(ChangeSlot.applyTo);
         annotatesSlot = model.getSlot(ChangeSlot.annotates);
-        partOfCompositeChangeSlot = model.getSlot(ChangeSlot.partOfCompositeChange);
     }
     
     public void ownSlotValueChanged(FrameEvent event) {
@@ -27,8 +27,9 @@ public abstract class AbstractChangeListener extends FrameAdapter {
             if (slot.equals(applyToSlot)) {
                 addChange(change);
             }
-            else if (change.getApplyTo() != null  && !slot.equals(partOfCompositeChangeSlot)) {
-                modifyChange(change);
+            else if (change.getApplyTo() != null) {
+                List oldValues = event.getOldValues();
+                modifyChange(change, slot, oldValues);
             }
         }
         else if (frame instanceof Annotation && slot.equals(annotatesSlot)) {
@@ -38,7 +39,7 @@ public abstract class AbstractChangeListener extends FrameAdapter {
     
     public abstract void addChange(Change change);
     
-    public abstract void modifyChange(Change change);
+    public abstract void modifyChange(Change change, Slot slot, List oldValues);
     
     public abstract void addAnnotation(Annotation annotation);
 
