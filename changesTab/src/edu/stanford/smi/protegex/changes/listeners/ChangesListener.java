b9699c923313cbbe727cf9cd3f04823fc06a453a
Tania Tudorache <tudorache@users.noreply.github.com>
2007-03-20 18:14:21 +0000

    destatified the ChangesTab

--- changesTab/src/edu/stanford/smi/protegex/changes/listeners/ChangesListener.java
+++ changesTab/src/edu/stanford/smi/protegex/changes/listeners/ChangesListener.java
@@ -8,7 +8,9 @@ import java.util.logging.Logger;
 
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.changes.ChangeTreeTableModel;
 import edu.stanford.smi.protegex.changes.ChangesTab;
+import edu.stanford.smi.protegex.changes.ui.ChangeMenu;
 import edu.stanford.smi.protegex.server_changes.model.AbstractChangeListener;
 import edu.stanford.smi.protegex.server_changes.model.ChangeModel;
 import edu.stanford.smi.protegex.server_changes.model.generated.Annotation;
@@ -18,19 +20,23 @@ import edu.stanford.smi.protegex.server_changes.model.generated.Change;
 public class ChangesListener extends AbstractChangeListener {
     private final static Logger log = Log.getLogger(ChangesListener.class);
     
-    public ChangesListener(ChangeModel model) {
+    private ChangesTab changesTab;
+    
+    public ChangesListener(ChangeModel model, ChangesTab changesTab) {
         super(model);
+        
+        this.changesTab = changesTab;
     }
     
     public void addChange(Change change) {
         ChangeModel.logAnnotatableThing("ChangeTab listener received change", log, Level.FINE, change);
-        ChangesTab.createChange(change);
+        changesTab.createChange(change);
     }
     
     @Override
     public void modifyChange(Change change, Slot slot, List oldValues) {
         ChangeModel.logAnnotatableThing("ChangeTab listener received modification to change", log, Level.FINE, change);
-        ChangesTab.modifyChange(change, slot, oldValues);
+        changesTab.modifyChange(change, slot, oldValues);
     }
     
     public void addAnnotation(Annotation a) { }
