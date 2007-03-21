851092887f7e61d30ea36fdc24e784446e139294
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-03-21 06:42:53 +0000

    added a check for null timestamps

--- change-management/trunk/model/src/edu/stanford/smi/protegex/server_changes/model/AnnotationCreationComparator.java
+++ change-management/trunk/model/src/edu/stanford/smi/protegex/server_changes/model/AnnotationCreationComparator.java
@@ -11,7 +11,15 @@ public class AnnotationCreationComparator implements Comparator<Instance> {
     public int compare(Instance arg0, Instance arg1) {
         Annotation a0 = (Annotation) arg0;
         Annotation a1 = (Annotation) arg1;
-        return ((Timestamp) a0.getCreated()).compareTimestamp((Timestamp) a1.getCreated());
+        
+        Timestamp timestamp0 = (Timestamp) a0.getCreated();
+        Timestamp timestamp1 = (Timestamp) a1.getCreated();
+        
+        if (timestamp0 == null) {
+        	return 1;
+        }
+        
+        return timestamp0.compareTimestamp(timestamp1);
     }
 
 }
