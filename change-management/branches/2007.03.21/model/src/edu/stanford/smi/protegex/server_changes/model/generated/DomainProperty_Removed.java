3ee61698e73384f8c5a021d5f6124ca5b39af7ea
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-03-22 19:40:33 +0000

    refactored Changes ontology

--- change-management/branches/2007.03.21/model/src/edu/stanford/smi/protegex/server_changes/model/generated/DomainProperty_Removed.java
+++ change-management/branches/2007.03.21/model/src/edu/stanford/smi/protegex/server_changes/model/generated/DomainProperty_Removed.java
@@ -1,5 +1,5 @@
 
-// Created on Wed Mar 21 17:52:27 PDT 2007
+// Created on Thu Mar 22 12:38:19 PDT 2007
 // "Copyright Stanford University 2006"
 
 package edu.stanford.smi.protegex.server_changes.model.generated;
@@ -16,6 +16,12 @@ public class DomainProperty_Removed extends Class_Change {
 
 	public DomainProperty_Removed(KnowledgeBase kb, FrameID id ) {
 		super(kb, id);
+	}
+
+	public void setAssociatedProperty(Instance associatedProperty) {
+		ModelUtilities.setOwnSlotValue(this, "associatedProperty", associatedProperty);	}
+	public Instance getAssociatedProperty() {
+		return ((Instance) ModelUtilities.getOwnSlotValue(this, "associatedProperty"));
 	}
 // __Code above is automatically generated. Do not change
 }
