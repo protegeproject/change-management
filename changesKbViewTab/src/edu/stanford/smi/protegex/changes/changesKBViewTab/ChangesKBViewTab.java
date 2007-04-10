5035c79959524a2cf0c9d9c0994f264f7f3abf7d
Tania Tudorache <tudorache@users.noreply.github.com>
2007-04-10 01:38:35 +0000

    Made it work also in client-server. Added dispose.

--- changesKbViewTab/src/edu/stanford/smi/protegex/changes/changesKBViewTab/ChangesKBViewTab.java
+++ changesKbViewTab/src/edu/stanford/smi/protegex/changes/changesKBViewTab/ChangesKBViewTab.java
@@ -3,9 +3,13 @@ package edu.stanford.smi.protegex.changes.changesKBViewTab;
 import java.awt.Container;
 import java.util.Collection;
 
+import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.model.framestore.FrameStoreManager;
+import edu.stanford.smi.protege.server.RemoteProjectManager;
+import edu.stanford.smi.protege.server.framestore.RemoteClientFrameStore;
 import edu.stanford.smi.protege.ui.DirectInstancesList;
 import edu.stanford.smi.protege.ui.FrameRenderer;
 import edu.stanford.smi.protege.util.Log;
@@ -13,12 +17,15 @@ import edu.stanford.smi.protege.util.SelectableList;
 import edu.stanford.smi.protege.widget.AbstractTabWidget;
 import edu.stanford.smi.protege.widget.ClsesAndInstancesTab;
 import edu.stanford.smi.protegex.server_changes.ChangesProject;
+import edu.stanford.smi.protegex.server_changes.GetAnnotationProjectName;
+import edu.stanford.smi.protegex.server_changes.model.generated.Ontology_Component;
 
 /**
  * @author Tania Tudorache <tudorache@stanford.edu>
  *
  */
 public class ChangesKBViewTab extends AbstractTabWidget {
+	//TODO: Clean up this class
 	
 	ClsesAndInstancesTab ciTab;
 	Project changesProject;
@@ -40,7 +47,7 @@ public class ChangesKBViewTab extends AbstractTabWidget {
 		ciTab.initialize();
 		
 		//getClsTree().setCellRenderer(new FrameIDRenderer());
-		adjustDirectInstanceRenderer();	
+		//adjustDirectInstanceRenderer();	
 		
 		add(ciTab);
 	};
@@ -54,10 +61,14 @@ public class ChangesKBViewTab extends AbstractTabWidget {
 		KnowledgeBase changesKB = null;
 		
 		if (getProject() != null) {
-			changesKB = ChangesProject.getChangesKB(getKnowledgeBase());
+			changesKB = getChangesKb(getKnowledgeBase());		  
 		}
 		
-		return (changesKB == null ? null : changesKB.getProject());
+		if (changesKB != null) {
+			changesProject = changesKB.getProject();
+		}
+		
+		return changesProject;
 	}	
 	
 	
@@ -96,5 +107,63 @@ public class ChangesKBViewTab extends AbstractTabWidget {
 				appendText(" id=" + ((Frame)value).getFrameID().getLocalPart());
 		}
 	}
+	
+	
+	//copied from ChangesTab. This should be unified at some point with the ChangesTab method!!!
+	private  KnowledgeBase getChangesKb(KnowledgeBase kb) {
+				
+		if (changesProject != null) {
+			return changesProject.getKnowledgeBase();
+		}
+		
+		KnowledgeBase changesKb = null;
+			
+		try {
+			// NEED TO ADD IMPLEMENTATION FOR SERVER MODE
+			// But this project must "essentially" be the same as the project that the project plugin is using
+			// same events, contents etc.
+			// it also runs after the changes project plugin has initialized.
+			if (kb.getProject().isMultiUserClient()) {
+				changesKb = getServerSideChangeKb(kb);
+			}
+			else {
+				changesKb = ChangesProject.getChangesKB(kb);		
+			}
+						
+		} catch(Throwable e) {
+			Log.getLogger().warning("There were errors at getting the Changes project attached to " + kb + ". Error message: " + e.getMessage());
+		}
+
+		return changesKb;
+	}
+	
+	private KnowledgeBase getServerSideChangeKb(KnowledgeBase kb) {
+		String annotationName = (String) new GetAnnotationProjectName(kb).execute();
+		if (annotationName == null) {
+			Log.getLogger().warning("annotation project not configured (use " +
+					GetAnnotationProjectName.METAPROJECT_ANNOTATION_PROJECT_SLOT +
+			" slot)");
+		}
+		RemoteProjectManager manager = RemoteProjectManager.getInstance();
+		FrameStoreManager fsmanager = ((DefaultKnowledgeBase) kb).getFrameStoreManager();
+		RemoteClientFrameStore rcfs = (RemoteClientFrameStore) fsmanager.getFrameStoreFromClass(RemoteClientFrameStore.class);
+		Project changes_project = manager.connectToProject(rcfs.getRemoteServer(), rcfs.getSession(), annotationName);
+		return changes_project.getKnowledgeBase();	
+	}
+	
+	@Override
+	public void dispose() {
+		if (changesProject != null) {			
+			try {
+				changesProject.dispose();
+			} catch (Exception e) {
+				Log.getLogger().warning("Errors at disposing changes project " + changesProject + " of project " + changesProject);
+			}
+		}	
+		
+		//TODO: remove the listeners		
+		super.dispose();
+	}
+	
 	
 }
