b7b4d0b19492219b77006593dbc50e97e72634d8
Tania Tudorache <tudorache@users.noreply.github.com>
2007-04-19 23:20:31 +0000

    added ChangeSlots

--- model/src/edu/stanford/smi/protegex/server_changes/model/ChangeModel.java
+++ model/src/edu/stanford/smi/protegex/server_changes/model/ChangeModel.java
@@ -126,6 +126,8 @@ public class ChangeModel {
         subChanges,
         timestamp,
         currentName,
+        subject,
+        voteValue,
     }
     
     public KnowledgeBase getChangeKb() {
