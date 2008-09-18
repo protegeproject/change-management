897064424156d60b6084f6350b5766ed4b332369
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-18 07:33:20 +0000

    adapt to new CHAO model and API

--- change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/postprocess/AnnotationCombiner.java
+++ change-management/trunk/projectPlugin/src/edu/stanford/smi/protegex/server_changes/postprocess/AnnotationCombiner.java
@@ -6,21 +6,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import edu.stanford.bmir.protegex.chao.change.api.Annotation_Change;
+import edu.stanford.bmir.protegex.chao.change.api.Annotation_Removed;
+import edu.stanford.bmir.protegex.chao.change.api.Change;
+import edu.stanford.bmir.protegex.chao.change.api.ChangeFactory;
+import edu.stanford.bmir.protegex.chao.change.api.Composite_Change;
+import edu.stanford.bmir.protegex.chao.ontologycomp.api.Ontology_Component;
 import edu.stanford.smi.protege.server.RemoteSession;
-import edu.stanford.smi.protegex.server_changes.ChangesDb;
-import edu.stanford.smi.protegex.server_changes.model.ChangeModel.ChangeCls;
-import edu.stanford.smi.protegex.server_changes.model.generated.Annotation_Change;
-import edu.stanford.smi.protegex.server_changes.model.generated.Annotation_Removed;
-import edu.stanford.smi.protegex.server_changes.model.generated.Change;
-import edu.stanford.smi.protegex.server_changes.model.generated.Composite_Change;
-import edu.stanford.smi.protegex.server_changes.model.generated.Ontology_Component;
+import edu.stanford.smi.protegex.server_changes.PostProcessorManager;
 
 public class AnnotationCombiner implements PostProcessor {
-    private ChangesDb changes_db;
+    private PostProcessorManager changes_db;
     private Map<RemoteSession, List<Annotation_Change>> lastAnnotationsBySession
                                     = new HashMap<RemoteSession, List<Annotation_Change>>();
-    
-    public void initialize(ChangesDb changes_db) {
+
+    public void initialize(PostProcessorManager changes_db) {
         this.changes_db = changes_db;
     }
 
@@ -30,7 +30,7 @@ public class AnnotationCombiner implements PostProcessor {
     public void addChange(Change aChange) {
         RemoteSession session = changes_db.getCurrentSession();
         List<Annotation_Change> previous_annotations = lastAnnotationsBySession.get(session);
-        
+
         if (aChange instanceof Annotation_Change) {
             Annotation_Change annotation = (Annotation_Change) aChange;
 
@@ -40,12 +40,12 @@ public class AnnotationCombiner implements PostProcessor {
                 lastAnnotationsBySession.put(session, previous_annotations);
                 return;
             }
-            
-            Ontology_Component applyTo = (Ontology_Component) annotation.getApplyTo();
-            Ontology_Component property = (Ontology_Component) annotation.getAssociatedProperty();
-            
+
+            Ontology_Component applyTo = annotation.getApplyTo();
+            Ontology_Component property = annotation.getAssociatedProperty();
+
             Annotation_Change earlier_annotation = previous_annotations.get(0);
-            if (!applyTo.equals(earlier_annotation.getApplyTo()) || 
+            if (!applyTo.equals(earlier_annotation.getApplyTo()) ||
                     !property.equals(earlier_annotation.getAssociatedProperty())) {
                 combineAnnotations(previous_annotations);
                 List<Annotation_Change> new_annotations = new ArrayList<Annotation_Change>();
@@ -61,21 +61,20 @@ public class AnnotationCombiner implements PostProcessor {
         else if (previous_annotations != null) {
             combineAnnotations(previous_annotations);
         }
-        
-    }
-    
-    private void combineAnnotations(List<Annotation_Change> annotations) {
-        lastAnnotationsBySession.remove(changes_db.getCurrentSession());
-        if (annotations.size() <= 1) return;
-        Ontology_Component applyTo = (Ontology_Component) annotations.get(0).getApplyTo();
-        Composite_Change transaction = (Composite_Change) changes_db.createChange(ChangeCls.Composite_Change);
-        
-        transaction.setSubChanges(annotations);
-        
-        changes_db.finalizeChange(transaction, applyTo, getContextForAnnotations(annotations));
 
     }
-    
+
+    private void combineAnnotations(List<Annotation_Change> annotations) {
+        lastAnnotationsBySession.remove(changes_db.getCurrentSession());
+        if (annotations.size() <= 1) {
+			return;
+		}
+        Ontology_Component applyTo = annotations.get(0).getApplyTo();
+        Composite_Change transaction = new ChangeFactory(changes_db.getChangesKb()).createComposite_Change(null);
+        transaction.setSubChanges(annotations);
+        changes_db.finalizeChange(transaction, applyTo, getContextForAnnotations(annotations));
+    }
+
     private String getContextForAnnotations(Collection<Annotation_Change> annotations) {
         String context = null;
         for (Annotation_Change annotation : annotations) {
