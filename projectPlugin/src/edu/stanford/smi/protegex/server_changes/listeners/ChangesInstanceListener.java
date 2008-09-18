1a36cc77c7ef99e66ebc076479bf978c8aa58a19
Tania Tudorache <tudorache@users.noreply.github.com>
2008-09-18 07:32:38 +0000

    adapt to new CHAO model and API

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/ChangesInstanceListener.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/ChangesInstanceListener.java
@@ -1,19 +1,21 @@
 package edu.stanford.smi.protegex.server_changes.listeners;
 
+import edu.stanford.bmir.protegex.chao.change.api.ChangeFactory;
 import edu.stanford.smi.protege.event.InstanceEvent;
 import edu.stanford.smi.protege.event.InstanceListener;
 import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protegex.server_changes.ChangesDb;
+import edu.stanford.smi.protegex.server_changes.PostProcessorManager;
 import edu.stanford.smi.protegex.server_changes.ChangesProject;
 import edu.stanford.smi.protegex.server_changes.ServerChangesUtil;
-import edu.stanford.smi.protegex.server_changes.model.ChangeModel.ChangeCls;
 
-public class ChangesInstanceListener implements InstanceListener{    
-    private ChangesDb changes_db;    
+public class ChangesInstanceListener implements InstanceListener{
+    private PostProcessorManager changes_db;
+    private ChangeFactory factory;
 
     public ChangesInstanceListener(KnowledgeBase kb) {
         changes_db = ChangesProject.getChangesDb(kb);
+        factory = new ChangeFactory(changes_db.getChangesKb());
     }
     /* (non-Javadoc)
      * @see edu.stanford.smi.protege.event.InstanceListener#directTypeAdded(edu.stanford.smi.protege.event.InstanceEvent)
@@ -21,7 +23,7 @@ public class ChangesInstanceListener implements InstanceListener{
     public void directTypeAdded(InstanceEvent event) {
 
         String directType = event.getInstance().getDirectType().getBrowserText();
-        
+
         Instance inst = event.getInstance();
         String instName = inst.getBrowserText();
 
@@ -31,15 +33,15 @@ public class ChangesInstanceListener implements InstanceListener{
         context.append(" (added to: ");
         context.append(instName);
         context.append(")");
-        
-        ServerChangesUtil.createChangeStd(changes_db, ChangeCls.DirectType_Added, inst, context.toString());
+
+        ServerChangesUtil.createChangeStd(changes_db, factory.createDirectType_Added(null), inst, context.toString());
     }
 
     /* (non-Javadoc)
      * @see edu.stanford.smi.protege.event.InstanceListener#directTypeRemoved(edu.stanford.smi.protege.event.InstanceEvent)
      */
-    public void directTypeRemoved(InstanceEvent event) {        
-        String directTypeText = event.getInstance().getDirectType().getBrowserText();        
+    public void directTypeRemoved(InstanceEvent event) {
+        String directTypeText = event.getInstance().getDirectType().getBrowserText();
         String instText = event.getInstance().getBrowserText();
 
         StringBuffer context = new StringBuffer();
@@ -48,7 +50,7 @@ public class ChangesInstanceListener implements InstanceListener{
         context.append(" (removed from: ");
         context.append(instText);
         context.append(")");
-        
-        ServerChangesUtil.createChangeStd(changes_db, ChangeCls.DirectType_Removed, event.getInstance(), context.toString());
+
+        ServerChangesUtil.createChangeStd(changes_db, factory.createDirectType_Removed(null), event.getInstance(), context.toString());
     }
 }
