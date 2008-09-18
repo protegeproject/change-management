6289821b0251aa4ab87382a123b858e87267336d
Tania Tudorache <tudorache@users.noreply.github.com>
2008-09-18 07:32:55 +0000

    adapt to new CHAO model and API

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesClassListener.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/listeners/owl/OwlChangesClassListener.java
@@ -1,33 +1,27 @@
 package edu.stanford.smi.protegex.server_changes.listeners.owl;
 
-import edu.stanford.smi.protege.model.Instance;
-import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protege.model.Model;
+import edu.stanford.bmir.protegex.chao.change.api.ChangeFactory;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.RDFProperty;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 import edu.stanford.smi.protegex.owl.model.RDFSClass;
 import edu.stanford.smi.protegex.owl.model.event.ClassAdapter;
-import edu.stanford.smi.protegex.server_changes.ChangesDb;
+import edu.stanford.smi.protegex.server_changes.PostProcessorManager;
 import edu.stanford.smi.protegex.server_changes.ChangesProject;
 import edu.stanford.smi.protegex.server_changes.ServerChangesUtil;
-import edu.stanford.smi.protegex.server_changes.model.ChangeModel;
-import edu.stanford.smi.protegex.server_changes.model.ChangeModel.ChangeCls;
-import edu.stanford.smi.protegex.server_changes.model.generated.Change;
-import edu.stanford.smi.protegex.server_changes.model.generated.Individual_Added;
-import edu.stanford.smi.protegex.server_changes.model.generated.Ontology_Component;
- 
+
 public class OwlChangesClassListener extends ClassAdapter {
     private OWLModel om;
-    private ChangesDb changes_db;
-    private KnowledgeBase changesKb;
-    
+    private PostProcessorManager changes_db;
+    private ChangeFactory factory;
+
     public OwlChangesClassListener(OWLModel om) {
         this.om = om;
         changes_db = ChangesProject.getChangesDb(om);
-        changesKb = changes_db.getChangesKb();
+        factory = new ChangeFactory(changes_db.getChangesKb());
     }
 
+	@Override
 	public void instanceAdded(RDFSClass arg0, RDFResource arg1) {
 		String instText = arg1.getBrowserText();
 		String instName = arg1.getName();
@@ -37,32 +31,34 @@ public class OwlChangesClassListener extends ClassAdapter {
 		context.append(" (instance of: " );
 		context.append(arg0.getBrowserText() );
 		context.append(")");
-        
-        ServerChangesUtil.createChangeStd(changes_db, ChangeCls.Individual_Added, arg1, context.toString());
+
+        ServerChangesUtil.createChangeStd(changes_db, factory.createIndividual_Added(null), arg1, context.toString());
 	}
 
+	@Override
 	public void instanceRemoved(RDFSClass arg0, RDFResource arg1) {
-            ChangesDb changesDb = ChangesProject.getChangesDb(om);
-            String instText = changesDb.getPossiblyDeletedBrowserText(arg1);
+            PostProcessorManager changesDb = ChangesProject.getChangesDb(om);
+            //String instText = changesDb.getPossiblyDeletedBrowserText(arg1);
+            String instText = arg1.getName();
             StringBuffer context = new StringBuffer();
             context.append("Instance Removed: ");
             context.append(instText);
             context.append(" (instance of: ");
             context.append(arg0.getBrowserText());
             context.append(")");
-            
-            Ontology_Component applyTo = changes_db.getOntologyComponent(arg1, true);
-            
-            Change change = changes_db.createChange(ChangeCls.Individual_Removed);
-            changes_db.finalizeChange(change, applyTo, context.toString());
+
+            ServerChangesUtil.createChangeStd(changesDb, factory.createIndividual_Removed(null), arg1, context.toString());
 	}
 
+	@Override
 	public void addedToUnionDomainOf(RDFSClass arg0, RDFProperty arg1) {
 	}
 
+	@Override
 	public void removedFromUnionDomainOf(RDFSClass arg0, RDFProperty arg1) {
 	}
 
+	@Override
 	public void subclassAdded(RDFSClass arg0, RDFSClass arg1) {
 		String clsName = arg1.getName();
 		String clsText = arg1.getBrowserText();
@@ -72,22 +68,24 @@ public class OwlChangesClassListener extends ClassAdapter {
 		context.append(" (added to: ");
 		context.append(arg0.getBrowserText());
 		context.append(")");
-        
-        ServerChangesUtil.createChangeStd(changes_db, ChangeCls.Subclass_Added, arg1, context.toString());
+
+        ServerChangesUtil.createChangeStd(changes_db, factory.createSubclass_Added(null), arg1, context.toString());
 	}
 
+	@Override
 	public void subclassRemoved(RDFSClass arg0, RDFSClass arg1) {
-            String clsName = changes_db.getPossiblyDeletedBrowserText(arg1);
+            //String clsName = changes_db.getPossiblyDeletedBrowserText(arg1);
             StringBuffer context = new StringBuffer();
             context.append("Subclass Removed: ");
-            context.append(clsName);
+            context.append(arg1.getName());
             context.append(" (removed from: ");
             context.append(arg0.getBrowserText());
             context.append(")");
-            
-            ServerChangesUtil.createChangeStd(changes_db, ChangeCls.Subclass_Removed, arg1, context.toString());
+
+            ServerChangesUtil.createChangeStd(changes_db, factory.createSubclass_Removed(null), arg1, context.toString());
 	}
 
+	@Override
 	public void superclassAdded(RDFSClass arg0, RDFSClass arg1) {
 		String clsName = arg1.getName();
 		String clsText = arg1.getBrowserText();
@@ -97,19 +95,20 @@ public class OwlChangesClassListener extends ClassAdapter {
 		context.append(" (added to: ");
 		context.append(arg0.getBrowserText());
 		context.append(")");
-        
-        ServerChangesUtil.createChangeStd(changes_db, ChangeCls.Superclass_Added, arg1, context.toString());
+
+        ServerChangesUtil.createChangeStd(changes_db, factory.createSuperclass_Added(null), arg1, context.toString());
 	}
 
+	@Override
 	public void superclassRemoved(RDFSClass arg0, RDFSClass arg1) {
-		String clsName = changes_db.getPossiblyDeletedBrowserText(arg1);
+		//String clsName = changes_db.getPossiblyDeletedBrowserText(arg1);
 		StringBuffer context = new StringBuffer();
 		context.append("Superclass Removed: ");
-		context.append(clsName);
+		context.append(arg1.getName());
 		context.append(" (removed from: ");
 		context.append(arg0.getBrowserText());
 		context.append(")");
-        
-        ServerChangesUtil.createChangeStd(changes_db, ChangeCls.Superclass_Removed, arg1, context.toString());
+
+        ServerChangesUtil.createChangeStd(changes_db, factory.createSuperclass_Removed(null), arg1, context.toString());
 	}
 }
