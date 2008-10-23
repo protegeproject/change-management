b64fe6c9c15f2a63563f9080bac2cdc1d8acfc04
Tania Tudorache <tudorache@users.noreply.github.com>
2008-10-23 20:27:48 +0000

    fixed isSuitable to work also in client mode

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/prompt/UsersTab.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/prompt/UsersTab.java
@@ -2,6 +2,7 @@ package edu.stanford.smi.protegex.server_changes.prompt;
 
 import java.util.Collection;
 
+import edu.stanford.bmir.protegex.chao.ChAOKbManager;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protege.widget.AbstractTabWidget;
@@ -11,28 +12,23 @@ public class UsersTab extends AbstractTabWidget {
 
     public void initialize() {
 		setLabel("UsersTab");
-		
+
 		AuthorManagement authorManagement = AuthorManagement.getAuthorManagement(null, getKnowledgeBase());
-		
+
 		if (authorManagement == null) {
 			Log.getLogger().warning("Could not initialize AuthorManagement.");
 			return;
 		}
-		
+
 		DiffUserView diffUserView = new DiffUserView(null, getKnowledgeBase());
 		diffUserView.setAuthorManagement(authorManagement);
-		
+
 		add(diffUserView);
 	}
-	
-	//TT: Until we fix it to work in multi-user client, too.
+
     @SuppressWarnings("unchecked")
     public static boolean isSuitable(Project p, Collection errors) {
-        if (p.isMultiUserClient()) {
-        	errors.add("Works only in stand-alone mode");
-        	return false;
-        }
-        return true;
+    	return ChAOKbManager.getChAOKb(p.getKnowledgeBase()) != null;
     }
 
 }
