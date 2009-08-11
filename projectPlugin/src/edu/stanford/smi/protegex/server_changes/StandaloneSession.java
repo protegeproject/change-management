6799fff65af750e2ab3041045a56921c9a09b676
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-08-11 06:27:41 +0000

    lost commit

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/StandaloneSession.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/StandaloneSession.java
@@ -13,17 +13,33 @@ public class StandaloneSession implements RemoteSession {
 	}
 	
 	private StandaloneSession() {}
+	
+	public int getId() {
+	    return -1;
+	}
 
-	public String getUserName() {
+	public boolean allowDelegation() {
+        return false;
+    }
+
+    public RemoteSession makeDelegate(String delegateUserName) {
+        throw new IllegalAccessError("delegation not allowed");
+    }
+
+    public String getUserName() {
 		return null;
 	}
 
-	public String getUserIpAddress() {
+	public String getRealUserName() {
+        return null;
+    }
+
+    public String getUserIpAddress() {
 		return null;
 	}
 
-        public int getSessionGroup() {
-            return 0;
-        }
+    public long getStartTime() {
+        return 0;
+    }
 
 }
