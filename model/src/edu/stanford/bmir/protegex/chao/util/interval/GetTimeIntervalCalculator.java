9b1303e85a27cc4be697c253436582e69d3d6d49
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-08-27 00:19:09 +0000

    the time interval utilities are now working - add the login time stuff later

--- model/src/edu/stanford/bmir/protegex/chao/util/interval/GetTimeIntervalCalculator.java
+++ model/src/edu/stanford/bmir/protegex/chao/util/interval/GetTimeIntervalCalculator.java
@@ -1,8 +1,14 @@
 package edu.stanford.bmir.protegex.chao.util.interval;
 
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Date;
+
 import edu.stanford.bmir.protegex.chao.ChAOKbManager;
+import edu.stanford.bmir.protegex.chao.change.api.Change;
 import edu.stanford.smi.protege.exception.ProtegeException;
 import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.util.LocalizeUtils;
 import edu.stanford.smi.protege.util.ProtegeJob;
 
 public class GetTimeIntervalCalculator extends ProtegeJob {
@@ -16,7 +22,12 @@ public class GetTimeIntervalCalculator extends ProtegeJob {
     @Override
     public RemoteTimeIntervalCalculator run() throws ProtegeException {
         KnowledgeBase changesKb = ChAOKbManager.getChAOKb(getKnowledgeBase());
-        return new TimeIntervalCalculator(changesKb);
+        try {
+            return new RemoteTimeIntervalCalculatorImpl(changesKb);
+        }
+        catch (RemoteException re) {
+            throw new ProtegeException(re);
+        }
     }
 
     
@@ -26,6 +37,46 @@ public class GetTimeIntervalCalculator extends ProtegeJob {
     }
     
     public static RemoteTimeIntervalCalculator get(KnowledgeBase kb) {
-        return new GetTimeIntervalCalculator(kb).execute();
+        if (kb.getProject().isMultiUserClient()) {
+            return new ClientTimeIntervalCalculator(ChAOKbManager.getChAOKb(kb),
+                                                    new GetTimeIntervalCalculator(kb).execute());
+        }
+        else {
+            return new TimeIntervalCalculator(ChAOKbManager.getChAOKb(kb));
+        }
+    }
+    
+    private static class ClientTimeIntervalCalculator implements RemoteTimeIntervalCalculator  {
+        private KnowledgeBase changesKb;
+        private RemoteTimeIntervalCalculator delegate;
+        
+        private ClientTimeIntervalCalculator(KnowledgeBase changesKb, RemoteTimeIntervalCalculator delegate) {
+            this.changesKb = changesKb;
+            this.delegate = delegate;
+        }
+
+        public Collection<Change> getTopLevelChanges(Date start, Date end) throws RemoteException {
+            Collection<Change> changes = delegate.getTopLevelChanges(start, end);
+            LocalizeUtils.localize(changes, changesKb);
+            return changes;
+        }
+
+        public Collection<Change> getTopLevelChangesAfter(Date d) throws RemoteException {
+            Collection<Change> changes = delegate.getTopLevelChangesAfter(d);
+            LocalizeUtils.localize(changes, changesKb);
+            return changes;
+        }
+
+        public Collection<Change> getTopLevelChangesBefore(Date d) throws RemoteException {
+            Collection<Change> changes = delegate.getTopLevelChangesBefore(d);
+            LocalizeUtils.localize(changes, changesKb);
+            return changes;
+        }
+        
+        public void dispose() throws RemoteException {
+            delegate.dispose();
+        }
+        
+        
     }
 }
