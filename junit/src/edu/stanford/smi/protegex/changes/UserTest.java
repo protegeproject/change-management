8bd7648047859530d23b7a42d71772dfc9178342
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-09-10 05:14:19 +0000

    test passes

--- junit/src/edu/stanford/smi/protegex/changes/UserTest.java
+++ junit/src/edu/stanford/smi/protegex/changes/UserTest.java
@@ -8,11 +8,15 @@ import edu.stanford.bmir.protegex.chao.ChAOKbManager;
 import edu.stanford.bmir.protegex.chao.ontologycomp.api.OntologyComponentFactory;
 import edu.stanford.bmir.protegex.chao.ontologycomp.api.Ontology_Class;
 import edu.stanford.bmir.protegex.chao.ontologycomp.api.User;
+import edu.stanford.bmir.protegex.chao.ontologycomp.api.impl.DefaultOntology_Class;
+import edu.stanford.bmir.protegex.chao.ontologycomp.api.impl.DefaultUser;
 import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
 import edu.stanford.smi.protegex.server_changes.ChangesProject;
+import edu.stanford.smi.protegex.server_changes.ServerChangesUtil;
 
 public class UserTest extends TestCase {
     private OWLModel model;
@@ -36,12 +40,44 @@ public class UserTest extends TestCase {
         model.dispose();
     }
     
-    public void testUserType() {
+    public void testUserGetCreate() {
+        String userName = "Timothy Redmond";
         OntologyComponentFactory factory = new OntologyComponentFactory(chaoKb);
-        Instance a = factory.getOntology_ClassClass().createDirectInstance("test");
-        assertTrue(a instanceof Ontology_Class);
-        Instance u = factory.getUserClass().createDirectInstance("Timothy Redmond");
-        assertTrue(u instanceof User);
+        assertNull(factory.getUser(userName));
+        assertNull(factory.getUser(userName));
+        factory.createUser(userName);
+        assertNotNull(factory.getUser(userName));
+    }
+    
+    public void testOntologyComponentRename() {
+        String userName = "Timothy Redmond";
+        
+        OntologyComponentFactory factory = new OntologyComponentFactory(chaoKb);
+        assertNull(factory.getUser(userName));
+        User tr = factory.createUser(userName);
+
+        OWLNamedClass cls1 = model.createOWLNamedClass("A");
+        String originalName = cls1.getName();
+        Ontology_Class changesCls = (Ontology_Class) ServerChangesUtil.getOntologyComponent(chaoKb, originalName);
+        tr.addWatchedEntity(changesCls);
+        assertTrue(tr.getWatchedEntity().size() == 1);
+        assertTrue(tr.getWatchedEntity().contains(changesCls));
+        assertTrue(changesCls.getWatchedBy().size() == 1);
+        assertTrue(changesCls.getWatchedBy().contains(tr));
+        
+        cls1  = (OWLNamedClass) cls1.rename(originalName + "2");
+        String newName = cls1.getName();
+        assertTrue(changesCls.getCurrentName().equals(newName));
+        assertNull(changesCls.getInitialName());
+        assertTrue(tr.getWatchedEntity().size() == 1);
+        assertTrue(tr.getWatchedEntity().contains(changesCls));
+        assertTrue(changesCls.getWatchedBy().size() == 1);
+        assertTrue(changesCls.getWatchedBy().contains(tr));
+        
+        Instance iUser = ((DefaultUser) tr).getWrappedProtegeInstance();
+        Instance iCls  = ((DefaultOntology_Class) changesCls).getWrappedProtegeInstance();
+        assertTrue(iUser.getDirectOwnSlotValues(factory.getWatchedEntitySlot()).contains(iCls));
+        assertTrue(iCls.getDirectOwnSlotValues(factory.getWatchedBySlot()).contains(iUser));
     }
     
 }
