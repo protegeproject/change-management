eb55a6713230308be022f2e4836e0ebc53696073
Tania Tudorache <tudorache@users.noreply.github.com>
2009-09-21 06:31:53 +0000

    format

--- projectPlugin/src/edu/stanford/smi/protegex/server_changes/ChangesProject.java
+++ projectPlugin/src/edu/stanford/smi/protegex/server_changes/ChangesProject.java
@@ -34,16 +34,16 @@ public class ChangesProject extends ProjectPluginAdapter {
     private static Map<KnowledgeBase, PostProcessorManager> postProcessorManagerMap = new HashMap<KnowledgeBase, PostProcessorManager>();
 
     /* ---------------------------- Project Plugin Interfaces ---------------------------- */
-	@Override
-	public void afterLoad(Project p) {
-		if (!isChangeTrackingEnabled(p) || p.isMultiUserClient()) {
-			return;
-		}
-		initialize(p);
-	}
+    @Override
+    public void afterLoad(Project p) {
+        if (!isChangeTrackingEnabled(p) || p.isMultiUserClient()) {
+            return;
+        }
+        initialize(p);
+    }
 
     @Override
-	public void afterSave(Project p) {
+    public void afterSave(Project p) {
         if (p.isMultiUserClient()) {
             return;
         }
@@ -51,31 +51,33 @@ public class ChangesProject extends ProjectPluginAdapter {
     }
 
     @Override
-	public void beforeClose(Project p) {
+    public void beforeClose(Project p) {
         if (p.isMultiUserClient()) {
             return;
         }
         KnowledgeBase kb = p.getKnowledgeBase();
-        PostProcessorManager postProcessorManager  = getPostProcessorManager(kb);
+        PostProcessorManager postProcessorManager = getPostProcessorManager(kb);
         if (postProcessorManager != null) {
             if (!p.isMultiUserServer()) {
-            	//FIXME: don't dispose for now. The ChAOKBManager should handle this.
+                //FIXME: don't dispose for now. The ChAOKBManager should handle this.
                 //changesDb.getChangesKb().dispose();
             }
             postProcessorManagerMap.remove(kb);
         }
+
+        //TODO: who removes the KB listeners on the client?!
     }
 
     /* ---------------------------- End of Project Plugin Interfaces ---------------------------- */
 
     public static boolean isChangeTrackingEnabled(Project p) {
-    	boolean trackChanges = p.getChangeTrackingActive();
+        boolean trackChanges = p.getChangeTrackingActive();
 
-    	if (trackChanges) {
-    		return true;
-    	}
+        if (trackChanges) {
+            return true;
+        }
 
-    	//If the update modification slots is not set, try to find the changes tab
+        //If the update modification slots is not set, try to find the changes tab
 
         String changesTabClassName = ChangesTab.class.getName();
         for (Object o : p.getTabWidgetDescriptors()) {
@@ -91,48 +93,47 @@ public class ChangesProject extends ProjectPluginAdapter {
         return postProcessorManagerMap.get(p.getKnowledgeBase()) != null;
     }
 
-	public static void initialize(Project p) {
-	    if (p.isMultiUserClient()) {
-	        return;
-	    }
-		Project currentProj = p;
-		KnowledgeBase currentKB = currentProj.getKnowledgeBase();
+    public static void initialize(Project p) {
+        if (p.isMultiUserClient()) {
+            return;
+        }
+        Project currentProj = p;
+        KnowledgeBase currentKB = currentProj.getKnowledgeBase();
 
         KnowledgeBase changesKb = ChAOKbManager.getChAOKb(currentKB);
-		if (changesKb == null) {
-			Log.getLogger().warning("Could not find the ChAO KB.");
-			return;
-		}
+        if (changesKb == null) {
+            Log.getLogger().warning("Could not find the ChAO KB.");
+            return;
+        }
 
-		installPostProcessors(currentKB);
+        installPostProcessors(currentKB);
 
-		//Check to see if the project is an OWL one
-		boolean isOwlProject = Util.kbInOwl(currentKB);
+        //Check to see if the project is an OWL one
+        boolean isOwlProject = Util.kbInOwl(currentKB);
 
-		if (p.isMultiUserServer()) {
-		    ServerFrameStore.requestEventDispatch(currentKB);
-		    ((DefaultKnowledgeBase) changesKb).setCacheMachine(new ChangeOntStateMachine(changesKb));
-		}
+        if (p.isMultiUserServer()) {
+            ServerFrameStore.requestEventDispatch(currentKB);
+            ((DefaultKnowledgeBase) changesKb).setCacheMachine(new ChangeOntStateMachine(changesKb));
+        }
 
-		// Register listeners
-		if (isOwlProject) {
-			ChangesProjectOWL.registerOwlListeners(currentKB);
-		} else {
-			registerKBListeners(currentKB);
-		}
-	}
+        // Register listeners
+        if (isOwlProject) {
+            ChangesProjectOWL.registerOwlListeners(currentKB);
+        } else {
+            registerKBListeners(currentKB);
+        }
+    }
 
-	private static void registerKBListeners(KnowledgeBase currentKB) {
-		currentKB.addKnowledgeBaseListener(new ChangesKBListener(currentKB));
-		currentKB.addClsListener(new ChangesClsListener(currentKB));
-		currentKB.addInstanceListener(new ChangesInstanceListener(currentKB));
-		currentKB.addSlotListener(new ChangesSlotListener(currentKB));
-		currentKB.addTransactionListener(new ChangesTransListener(currentKB));
-		currentKB.addFrameListener(new ChangesFrameListener(currentKB));
-	}
+    private static void registerKBListeners(KnowledgeBase currentKB) {
+        currentKB.addKnowledgeBaseListener(new ChangesKBListener(currentKB));
+        currentKB.addClsListener(new ChangesClsListener(currentKB));
+        currentKB.addInstanceListener(new ChangesInstanceListener(currentKB));
+        currentKB.addSlotListener(new ChangesSlotListener(currentKB));
+        currentKB.addTransactionListener(new ChangesTransListener(currentKB));
+        currentKB.addFrameListener(new ChangesFrameListener(currentKB));
+    }
 
-
-	private static void installPostProcessors(KnowledgeBase currentKB) {
+    private static void installPostProcessors(KnowledgeBase currentKB) {
         PostProcessorManager ppm = postProcessorManagerMap.get(currentKB);
         if (ppm == null) {
             ppm = new PostProcessorManager(currentKB);
@@ -141,39 +142,39 @@ public class ChangesProject extends ProjectPluginAdapter {
             ppm.addPostProcessor(new JoinInstanceCreateAndAdd());
             postProcessorManagerMap.put(currentKB, ppm);
         }
-	}
+    }
 
-
-	public static String getUserName(KnowledgeBase currentKB) {
-		return currentKB.getUserName();
-	}
+    public static String getUserName(KnowledgeBase currentKB) {
+        return currentKB.getUserName();
+    }
 
     public static PostProcessorManager getPostProcessorManager(KnowledgeBase kb) {
         return postProcessorManagerMap.get(kb);
     }
 
-	public static KnowledgeBase getChangesKB(KnowledgeBase kb) {
+    public static KnowledgeBase getChangesKB(KnowledgeBase kb) {
         return ChAOKbManager.getChAOKb(kb);
-	}
+    }
 
-	public static Project getChangesProj(KnowledgeBase kb) {
-		KnowledgeBase chaoKb = getChangesKB(kb);
-		return chaoKb == null ? null : chaoKb.getProject();
-	}
+    public static Project getChangesProj(KnowledgeBase kb) {
+        KnowledgeBase chaoKb = getChangesKB(kb);
+        return chaoKb == null ? null : chaoKb.getProject();
+    }
 
-	@Override
-	public String getName() {
-		return "Changes Project Plugin";
-	}
+    @Override
+    public String getName() {
+        return "Changes Project Plugin";
+    }
 
-	public static void displayErrors(Collection errors) {
+    public static void displayErrors(Collection errors) {
         Iterator i = errors.iterator();
         while (i.hasNext()) {
             Object elem = i.next();
             if (elem instanceof Throwable) {
-                Log.getLogger().log(Level.WARNING, "Warnings at loading changes project", (Throwable)elem);
+                Log.getLogger().log(Level.WARNING, "Warnings at loading changes project", (Throwable) elem);
             } else if (elem instanceof MessageError) {
-                Log.getLogger().log(Level.WARNING, ((MessageError)elem).getMessage(), ((MessageError)elem).getException());
+                Log.getLogger().log(Level.WARNING, ((MessageError) elem).getMessage(),
+                        ((MessageError) elem).getException());
             } else {
                 Log.getLogger().warning(elem.toString());
             }
@@ -181,7 +182,7 @@ public class ChangesProject extends ProjectPluginAdapter {
     }
 
     public static void main(String[] args) {
-		Application.main(args);
-	}
+        Application.main(args);
+    }
 
 }
