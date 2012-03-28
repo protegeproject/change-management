078d72a6b44c3f852f9dea0ee6eb210840dd272b
nyulas <2441046+csnyulas@users.noreply.github.com>
2012-03-28 05:42:18 +0000

    Fixed syntax of table ENGINE and CHARACTER SET specification for CREATE TABLE command base on a SELECT.

--- etc/delete_all_subchanges_with_inner_joins_v2.sql
+++ etc/delete_all_subchanges_with_inner_joins_v2.sql
@@ -12,7 +12,7 @@ BEGIN
 #create copy of table to work on
 DROP TABLE IF EXISTS tcck_ann2;
 
-SET @v_command = CONCAT('CREATE TABLE tcck_ann2 AS SELECT * FROM ', tablename, ' ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;');
+SET @v_command = CONCAT('CREATE TABLE tcck_ann2  ENGINE = InnoDB, DEFAULT CHARACTER SET = utf8 AS SELECT * FROM ', tablename, ';');
 PREPARE stmt3 FROM @v_command;
 EXECUTE stmt3;
 DEALLOCATE PREPARE stmt3;
