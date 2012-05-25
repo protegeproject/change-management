11e7ec743905abc6174ae6531095741201071708
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2012-05-25 20:57:54 +0000

    added stuff

--- change-management/trunk/changesUtil/src/edu/stanford/bmir/protegex/changes/export/NotesExport.java
+++ change-management/trunk/changesUtil/src/edu/stanford/bmir/protegex/changes/export/NotesExport.java
@@ -11,14 +11,18 @@ import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import edu.stanford.bmir.protegex.chao.annotation.api.AnnotatableThing;
 import edu.stanford.bmir.protegex.chao.annotation.api.Annotation;
 import edu.stanford.bmir.protegex.chao.annotation.api.AnnotationFactory;
+import edu.stanford.bmir.protegex.chao.annotation.api.OntologyJavaMapping;
+import edu.stanford.bmir.protegex.chao.ontologycomp.api.Ontology_Component;
 import edu.stanford.bmir.protegex.chao.ontologycomp.api.Timestamp;
 import edu.stanford.smi.protege.code.generator.wrapping.AbstractWrappedInstance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.storage.database.DatabaseKnowledgeBaseFactory;
 import edu.stanford.smi.protege.util.CollectionUtilities;
+import edu.stanford.smi.protegex.server_changes.ServerChangesUtil;
 
 
 public class NotesExport {
@@ -33,15 +37,18 @@ public class NotesExport {
     private String dbTable = null;
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 3) {
+        if (args.length < 2) {
             log.severe("First argument should be the file or project URI, " +
-                    "second argument should be the path to the exported file," +
-            "third argument can be the project change filter (NCI | ICD | something else)");
+                    "second argument should be the path to the exported file");
             System.exit(1);
         }
 
         String chaoPrjPath = args[0];
         String exportFilePath = args[1];
+
+        //init Java mappings
+        OntologyJavaMapping.initMap();
+        edu.stanford.bmir.protegex.chao.ontologycomp.api.OntologyJavaMapping.initMap();
 
         NotesExport exporter = new NotesExport();
 
@@ -101,7 +108,8 @@ public class NotesExport {
 
     private void printHeader(Writer w) throws IOException {
         w.write("note_subject" + SEPARATOR + "note_type" + SEPARATOR +
-                 "entity" + SEPARATOR + "author" + SEPARATOR + "timestamp" +
+                 "top_annotated_entity" + SEPARATOR + "direct_annotated_id" + SEPARATOR +
+                 "author" + SEPARATOR + "timestamp" +
                 SEPARATOR + "note_id" + SEPARATOR + "db_table" + "\n");
     }
 
@@ -109,13 +117,16 @@ public class NotesExport {
     private void printNote(Annotation note, Writer w) throws IOException {
         StringBuffer text = new StringBuffer();
 
-        text.append(quote(note.getBody()));
+        text.append(quote(note.getSubject()));
         text.append(SEPARATOR);
 
         text.append(getNoteType(note));
         text.append(SEPARATOR);
 
-        text.append(CollectionUtilities.getFirstItem(note.getAnnotates())); //FIXME
+        text.append(getNoteAttachedToOC(note));
+        text.append(SEPARATOR);
+
+        text.append(getNoteDirectAnnotatesId(note));
         text.append(SEPARATOR);
 
         text.append(note.getAuthor());
@@ -135,11 +146,28 @@ public class NotesExport {
     }
 
     private String getNoteType(Annotation note) {
-        return note.getClass().getSimpleName();
+        String name = note.getClass().getSimpleName();
+        return name.replace("Default", "");
     }
 
     private String quote(String s) {
+        if (s == null) { return ""; }
         return QUOTE_CHAR + s.replaceAll("\\" + QUOTE_CHAR, QUOTE_CHAR + QUOTE_CHAR) + QUOTE_CHAR;
+    }
+
+    private String getNoteAttachedToOC(Annotation note) {
+        Collection<Ontology_Component> ocs = ServerChangesUtil.getAnnotatedOntologyComponents(note);
+        if (ocs.size() > 0) {
+            Ontology_Component oc = CollectionUtilities.getFirstItem(ocs);
+            return oc == null ? "" : oc.getCurrentName();
+        }
+        return "";
+    }
+
+    private String getNoteDirectAnnotatesId(Annotation note) {
+        Collection<AnnotatableThing> anns = note.getAnnotates();
+        AnnotatableThing ann = CollectionUtilities.getFirstItem(anns);
+       return ((AbstractWrappedInstance)ann).getName();
     }
 
 }
