6d5b8bf98337f2555bf3607041510676d8d36b23
nyulas <2441046+csnyulas@users.noreply.github.com>
2012-06-12 07:56:58 +0000

    Added timestamp to the CSV export of the "Create class" ChAO entries.

--- etc/get_created_and_assigned_script.sql
+++ etc/get_created_and_assigned_script.sql
@@ -6,11 +6,13 @@
 
 SELECT icd_sl.short_value AS sorting_label, icd_title_label.short_value AS title,
        change_authors.short_value AS author, icd_pt.short_value AS assignedPrimaryTAG,
-       oc.short_value AS created_class_id     
+       oc.short_value AS created_class_id, ts.short_value AS timestamp
      FROM icd_ann AS all_changes 
   JOIN icd_ann AS change_authors ON change_authors.slot='author' AND all_changes.frame=change_authors.frame
   JOIN icd_ann AS change_applyto ON change_applyto.slot='applyTo' AND all_changes.frame=change_applyto.frame
   JOIN icd_ann AS oc ON oc.slot='currentName' AND change_applyto.short_value=oc.frame
+  LEFT JOIN icd_ann AS change_timestamp ON change_timestamp.slot='timestamp' AND all_changes.frame=change_timestamp.frame
+  JOIN icd_ann AS ts ON ts.slot='date' AND change_timestamp.short_value=ts.frame
   LEFT JOIN icd_umbrella AS icd_pt ON icd_pt.slot='http://who.int/icd#assignedPrimaryTAG' AND oc.short_value=icd_pt.frame
   LEFT JOIN icd_umbrella AS icd_sl ON icd_sl.slot='http://who.int/icd#sortingLabel' AND oc.short_value=icd_sl.frame
   JOIN icd_umbrella AS icd_title_id ON icd_title_id.slot='http://who.int/icd#icdTitle' AND oc.short_value=icd_title_id.frame
